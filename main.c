1a0a49db376843551022783686de6baef78ae262
Ojash042 <ojash25@gmail.com>
2025-06-06 20:46:10 +0545

    Added display command

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include "./log.h"
+// ReSharper disable CppDFANullDereference
+#include "./lib/log.h"
 #include <raylib.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,13 +11,12 @@
 #define DISPLAY_WIDTH 64
 #define DISPLAY_HEIGHT 32
 #define VARIABLE_REGISTER_SIZE 16
+#define DISPLAY_SCALE 10
 
 #define ROM_BASE_ADDRESS 0x200
 
-#define SCREEN_WIDTH_MULTIPLE 10
-#define SCREEN_HEIGHT_MULTIPLE 10
-
-typedef struct {
+typedef struct
+{
   u_int8_t V0;
   u_int8_t V1;
   u_int8_t V2;
@@ -36,23 +35,24 @@ typedef struct {
   u_int8_t VF;
 } GeneralRegister;
 
-typedef struct {
+typedef struct
+{
   u_int8_t memory[MEMORY_SIZE];
-  GeneralRegister *Generalregister;
+  GeneralRegister *generalRegister;
   u_int16_t program_counter;
   u_int16_t index_register;
   u_int16_t stack_register;
   u_int16_t stack[STACK_SIZE];
   u_int8_t delay_timer;
   u_int8_t sound_timer;
-  bool display[DISPLAY_WIDTH][DISPLAY_HEIGHT];
+  bool display[DISPLAY_HEIGHT][DISPLAY_WIDTH];
 } Chip;
 
-static Chip *chip = NULL;
-bool force_close = false;
+static Chip *chip = nullptr;
 
-void copy_font_to_memory(Chip *chip) {
-  const int FONT_BASE_MEMORY_ADDRESS = 0x50;
+void copy_font_to_memory(Chip *chip)
+{
+  constexpr int FONT_BASE_MEMORY_ADDRESS = 0x50;
   int font[] = {
       0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
       0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -73,113 +73,153 @@ void copy_font_to_memory(Chip *chip) {
   };
 
   memcpy(chip->memory + FONT_BASE_MEMORY_ADDRESS, font, sizeof(font));
-  for (int i = 0; i < sizeof(font) / sizeof(font[0]); i++) {
+  for (int i = 0; i < sizeof(font) / sizeof(font[0]); i++)
+  {
     chip->memory[FONT_BASE_MEMORY_ADDRESS + i] = font[i];
   }
 }
 
-int load_rom() {
+size_t load_rom()
+{
   FILE *rom = fopen("./roms/IBM_Logo.ch8", "rb");
-  if (rom == NULL) {
+  if (rom == NULL)
+  {
     logevent(Fatal, 64, "Error while reading Rom %s", "");
     return -1;
   }
 
   fseek(rom, 0, SEEK_END);
-  long length = ftell(rom);
+  const long length = ftell(rom);
   fseek(rom, 0, SEEK_SET);
-  unsigned char *file_data =
+  const auto file_data =
       (unsigned char *)(malloc(sizeof(unsigned char) * length));
 
-  size_t buffer_read = fread(file_data, 1, length, rom);
+  const size_t buffer_read = fread(file_data, 1, length, rom);
   memcpy(chip->memory + ROM_BASE_ADDRESS, file_data, buffer_read);
+  free(file_data);
 
   return buffer_read;
 }
 
-void parse_instruction() {}
+void parse_instruction(u_int8_t code)
+{
+}
 
-void setup_chip() {
-  chip->Generalregister = (GeneralRegister *)malloc(sizeof(GeneralRegister));
-  for (int i = 0; i <= 0xF; i++) {
-    *((u_int8_t *)chip->Generalregister + (i)) = 0;
+void setup_chip()
+{
+  chip->generalRegister = (GeneralRegister *)malloc(sizeof(GeneralRegister));
+  for (int i = 0; i <= 0xF; i++)
+  {
+    *((u_int8_t *)chip->generalRegister + (i)) = 0;
   }
+  memset(chip->memory, 0, sizeof(chip->memory));
+  memset(chip->display, 0, sizeof(chip->display));
+
   copy_font_to_memory(chip);
 }
 
-int main(int argv, char **argc) {
+int main(int argv, char **argc)
+{
 
   LogSettings logSettings = {
       Warn,  // FILE
-      Debug, // Console
-      Daily,
+      Warn, // Console
+      Daily // Interval,
   };
 
   setup_logs(&logSettings);
   logevent(Info, 64, "Starting Chip-8 Emulator...", "");
 
   chip = (Chip *)(malloc(sizeof(Chip)));
+  if (chip == nullptr)
+    exit(-1);
   setup_chip();
-  u_int16_t rom_size = load_rom();
+  const u_int16_t rom_size = load_rom();
 
   SetTraceLogLevel(LOG_NONE);
   SetTargetFPS(60);
-  InitWindow(DISPLAY_WIDTH * SCREEN_WIDTH_MULTIPLE,
-             DISPLAY_HEIGHT * SCREEN_HEIGHT_MULTIPLE, "CHIP-8");
+  InitWindow(DISPLAY_WIDTH * DISPLAY_SCALE , DISPLAY_HEIGHT * DISPLAY_SCALE,
+             "CHIP-8");
 
-  while (!WindowShouldClose()) {
+  while (!WindowShouldClose())
+  {
     BeginDrawing();
     ClearBackground(BLACK);
 
     chip->program_counter = (u_int16_t)0x200;
 
     while (chip->program_counter <
-           (u_int16_t)(rom_size + ROM_BASE_ADDRESS) - 1) {
-      // logevent(Debug, 64, "Reading.. 0x%04x", chip->program_counter);
+           (u_int16_t)(rom_size + ROM_BASE_ADDRESS) - 1)
+    {
 
-      u_int16_t code = (chip->memory[chip->program_counter] << 8) +
+      const u_int16_t code = (chip->memory[chip->program_counter] << 8) +
                        chip->memory[chip->program_counter + 1];
 
-      if (code == 0x00e0) {
+      if (code == 0x00e0)
+      {
         ClearBackground(BLACK);
-        // continue;
       }
 
-      logevent(Debug, 64, "0x%04x", code);
-      if ((code >> 12) == 0x1) {
-        logevent(Info, 64, "Program Counter Jumped to: 0x%04x",
-                 (code & 0x0FFF));
+      else if ((code >> 12) == 0x1)
+      {
+        logevent(Info, 64, "Program Counter Jumped to: 0x%04x", (code & 0x0FFF));
         chip->program_counter = (code & 0xFFF);
       }
 
-      if ((code >> 12) == 0x6) {
-        *((u_int8_t *)chip->Generalregister + ((code >> 8) & 0x0F)) =
-            code & 0x00FF;
-      }
-      if ((code >> 12) == 0x7) {
-        *((u_int8_t *)chip->Generalregister + ((code >> 8) & 0x0F)) +=
-            (u_int8_t)(code & 0x00FF);
-      }
-      if ((code >> 12) == 0xA) {
-        chip->index_register = code & 0x0FFF;
-      }
-      if ((code >> 12) == 0xD) {
-        // DXYN
-        u_int8_t x =
-            *((u_int8_t *)chip->Generalregister + ((code >> 8) & 0x00F));
-        u_int8_t y =
-            *((u_int8_t *)chip->Generalregister + ((code >> 4) & 0x0F));
-        u_int8_t n = (code & 0x000F);
-        logevent(Info, 64, "Drawing At (%01X, %01X), for %d", x, y, n);
-        // memcopy memory[IR] to memory[IR + N];
+      else if ((code >> 12) == 0x6)
+      {
+        *((u_int8_t *)chip->generalRegister + ((code >> 8) & 0x0F)) = code & 0x00FF;
       }
 
-      chip->program_counter += 2;
+      else if ((code >> 12) == 0x7)
+      {
+        *((u_int8_t *)chip->generalRegister + ((code >> 8) & 0x0F)) +=
+            (u_int8_t)(code & 0x00FF);
+      }
+
+      else if ((code >> 12) == 0xA)
+      {
+        chip->index_register = code & 0x0FFF;
+      }
+
+      else if ((code >> 12) == 0xD)
+      {
+        // DXYN
+        const u_int8_t x =
+            *((u_int8_t *)chip->generalRegister + ((code >> 8) & 0x00F)) % DISPLAY_WIDTH;
+        const u_int8_t y =
+            *((u_int8_t *)chip->generalRegister + ((code >> 4) & 0x0F)) % DISPLAY_HEIGHT;
+        u_int8_t const n = code & 0x000F;
+
+        for (int row = 0; row < n; row++) {
+          const u_int8_t sprite_byte = chip->memory[chip->index_register + row];
+          const int py =  (y + row) % DISPLAY_HEIGHT;
+          for (int bitPosition = 0; bitPosition < 8; bitPosition++) {
+            const int px =  (x + bitPosition) % DISPLAY_WIDTH;
+            const bool sprite_pixel = (sprite_byte & (0x80 >> bitPosition)) !=0;
+            if (sprite_pixel) {
+              if (chip->display[py][px]) {
+                chip->generalRegister->VF = 1;
+              }
+              chip->display[py][px] ^= 1;
+            }
+          }
+        }
+      }
+      for (int y= 0; y < DISPLAY_HEIGHT; y++)
+      {
+        for (int x = 0; x < DISPLAY_WIDTH; x++)
+        {
+          if (chip->display[y][x])
+            DrawRectangle(x * DISPLAY_SCALE, y * DISPLAY_SCALE, DISPLAY_SCALE,
+                          DISPLAY_SCALE, RAYWHITE);
+        }
+      }
+      chip->program_counter += 2; 
     }
     logevent(Info, 64, "Ended");
 
     EndDrawing();
-    CloseWindow();
   }
 
   logevent(Info, 64, "Ending Chip-8 Emulator...", "");
